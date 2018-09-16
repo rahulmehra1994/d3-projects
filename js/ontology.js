f82d1201d5d4f14936923a2e6753c9b36dbc903f
Rahul Mehra <rhlmehra1994@gmail.com>
2018-09-16 12:17:04 +0530

    enhanced ontology layout

--- js/ontology.js
+++ js/ontology.js
@@ -1,72 +1,183 @@
 var data = [
   {
     n1: [
-      ["n2", "in"],
-      ["n3", "out"],
-      ["n4", "in"],
-      ["n5", "out"],
-      ["n6", "in"],
-      ["n7", "out"]
+      ["1", "in"],
+      ["2", "out"],
+      ["3", "in"],
+      ["4", "in"],
+      ["5", "out"],
+      ["6", "in"],
+      ["7", "in"],
+      ["8", "out"],
+      ["9", "in"],
+      ["10", "in"],
+      ["5", "out"],
+      ["6", "in"],
+      ["7", "in"],
+      ["8", "out"],
+      ["9", "in"],
+      ["10", "in"],
+      ["5", "out"],
+      ["6", "in"],
+      ["7", "in"],
+      ["8", "out"],
+      ["9", "in"],
+      ["10", "in"],
+      ["5", "out"],
+      ["6", "in"],
+      ["7", "in"],
+      ["8", "out"],
+      ["9", "in"],
+      ["10", "in"],
+      ["5", "out"],
+      ["6", "in"],
+      ["7", "in"],
+      ["8", "out"],
+      ["9", "in"],
+      ["10", "in"],
+      ["5", "out"],
+      ["6", "in"],
+      ["7", "in"],
+      ["8", "out"],
+      ["9", "in"],
+      ["10", "in"],
+      ["5", "out"],
+      ["6", "in"],
+      ["7", "in"],
+      ["8", "out"],
+      ["9", "in"],
+      ["10", "in"],
+      ["5", "out"],
+      ["6", "in"],
+      ["7", "in"],
+      ["8", "out"],
+      ["9", "in"],
+      ["10", "in"],
+      ["5", "out"],
+      ["6", "in"],
+      ["7", "in"],
+      ["8", "out"],
+      ["9", "in"],
+      ["10", "in"]
     ]
   }
-  // { n2: [["n3", "in"], ["n4", "out"], ["n3", "in"], ["n4", "out"]] },
-  // { n2: [["n3", "in"], ["n4", "out"], ["n3", "in"]] }
 ];
 
+var width = 1000;
+var height = 1000;
 //Make an SVG Container
 var svgContainer = d3
   .select("body")
   .append("svg")
-  .attr("width", 1200)
-  .attr("height", 800);
-
-var originX = 200;
-var originY = 200;
-var innerCircleRadius = 20;
-var outerCircleRadius = 5;
+  .attr("width", width)
+  .attr("height", height);
 
 var tempData = data[0][Object.keys(data[0])[0]];
 var offset = 30 * [tempData.length * 0.2];
+var len = tempData.length;
+
+var originX = 350;
+var originY = 350;
+var innerCircleRadius = 20;
+var outerCircleRadius = 10;
+var offset = 30;
+var angle = 10;
+var radius = 10;
+var basicOffset = 0;
+
+var gap = 6;
+
+var chunkSize = (outerCircleRadius * 2) + (gap * 2);
+
+function adjuster(r) {
+  var circum = 2 * Math.PI * r;
+  var count = circum / chunkSize;
+  console.log("adjuster ", r, chunkSize, circum / chunkSize, len);
+  if (count > len) {
+    radius = r;
+    return;
+  } else {
+    adjuster(r + 1);
+  }
+}
+
+adjuster(radius);
+console.log(radius);
+
+var scaleX = d3.scale
+  .linear()
+  .domain([0, tempData.length])
+  .range([0, width]);
+
+var scaleY = d3.scale
+  .linear()
+  .domain([0, tempData.length])
+  .range([0, height]);
+
 var circles = svgContainer
   .selectAll("circle")
   .data(tempData)
   .enter()
   .append("circle")
   .attr("cx", function(d, i) {
-    console.log(d);
-    return originX + (innerCircleRadius + offset) * Math.sin(i);
+    return originX + (radius + basicOffset) * Math.sin((2 * Math.PI * i) / len);
   })
   .attr("cy", function(d, i) {
-    return originY - (innerCircleRadius + offset) * Math.cos(i);
+    return originY - (radius + basicOffset) * Math.cos((2 * Math.PI * i) / len);
   })
   .attr("r", function(d, i) {
     return outerCircleRadius;
   });
 
-  var text = svgContainer.selectAll("text")
-                        .data(tempData)
-                        .enter()
-                        .append("text");
+var text = svgContainer
+  .selectAll("text")
+  .data(tempData)
+  .enter()
+  .append("text");
 
-var offsetText = 30 * [tempData.length * 0.3];
+basicOffset = 20;
 
 var texts = text
-  .attr("x",  function(d, i) {
+  .attr("x", function(d, i) {
     console.log(d);
-    return originX + (innerCircleRadius + offsetText) * Math.sin(i);
+    return originX + (radius + basicOffset) * Math.sin((2 * Math.PI * i) / len);
   })
   .attr("y", function(d, i) {
-    return originY - (innerCircleRadius + offsetText) * Math.cos(i);
+    return originY - (radius + basicOffset) * Math.cos((2 * Math.PI * i) / len);
   })
-  .text(function(d) {
-    return d[0];
+  .text(function(d, i) {
+    return i;
   })
   .attr("font-family", "sans-serif")
   .attr("font-size", "12px")
-  .attr("fill", "red");
+  .attr("fill", "red")
+  .attr("text-anchor", 'middle')
+  .attr("transform", 'translate(0, 5)');
+
+
+  var lines = svgContainer
+  .selectAll("line")
+  .data(tempData)
+  .enter()
+  .append("line")
+  .attr("x1", originX)  
+  .attr("y1", originY)  
+  .attr("x2", function(d, i) {
+    return originX + (radius) * Math.sin((2 * Math.PI * i) / len);
+  })  
+  .attr("y2", function(d, i) {
+    return originY - (radius) * Math.cos((2 * Math.PI * i) / len);
+  })  
+  .attr("stroke","red")  
+  .attr("stroke-width",2)  
+  .attr("marker-end","url(#arrow)");  
+
+
+
 
 svgContainer
   .append("circle")
   .attr("cx", originX)
   .attr("cy", originY)
-  .attr("r", 20);
+  .attr("r", radius * 0.5)
+  .fill('color', 'lightgrey');
